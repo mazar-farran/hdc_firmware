45e0c7cd0aaa11984c40332768e2b49e1994eb3e
Chris Niessl <niessl@hellbender.com>
2022-04-02 18:46:32 -0400

    Update bridge package to use wrapper shell script and associated systemd service

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = 76e09ef5ec49b81094ca6298ca524d26c45c2fa3
+CAMERA_BRIDGE_VERSION = ea959d453c69299d5ac1a7ebc0439f63ef8171e5
 CAMERA_BRIDGE_SITE = $(call github,CapableRobot,capable_camera_firmware,$(CAMERA_BRIDGE_VERSION))
-CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"
+CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
+                          -DINSTALL_CONFIG_FILES_PATH="/opt/dashcam/bin/"
 # Looks like the camera-bridge links against libjpeg but the best way to provide
 # this is with the jpeg-turbo package.
 CAMERA_BRIDGE_DEPENDENCIES = boost libcamera jpeg-turbo
