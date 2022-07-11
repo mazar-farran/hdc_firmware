fe0a94498bb83e99e69964c5d945f0a530148681
Gunnar Ryder <gunnar@hellbender.com>
2022-07-11 20:19:10 +0000

    Add GPSD and logger packages to buildroot
    Added needed configuration for new packages

---
+++ dashcam/package/gnss-logger/gnss-logger.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# gnss-logger
+#
+################################################################################
+
+GNSS_LOGGER_VERSION = f147c2ae7052082b48390bac5a27f1c8a73292af
+GNSS_LOGGER_SITE = ssh://git@bitbucket.org/chr1sniessl/capable_camera_firmware-mirror.git
+GNSS_LOGGER_SITE_METHOD = git
+GNSS_LOGGER_CONF_OPTS = -DCMAKE_INSTALL_PREFIX="/opt/dashcam" \
+						-DINSTALL_CONFIG_FILES_PATH="/opt/dashcam/bin/"
+GNSS_LOGGER_DEPENDENCIES = boost json-for-modern-cpp
+GNSS_LOGGER_SUBDIR = gnss
+
+define GNSS_LOGGER_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/gnss-logger/gnss-logger.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/gnss-logger.service
+endef
+
+$(eval $(cmake-package))
