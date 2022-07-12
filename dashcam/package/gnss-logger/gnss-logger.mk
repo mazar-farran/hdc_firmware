43328b6d3f39591e4bea2faa82a38f48f3e3e8ed
Gunnar Ryder <gunnar@hellbender.com>
2022-07-12 21:32:23 +0000

    Updates for bug fixes and configuration
    - Updated buildroot config to add gnss-logger package
    - Updated gnss-logger package to use a config file
    - Added enhancements to better use configurations (create logging directory if not present)
    - Bug fixes in project repo to ensure data is only logged when location is fixed

--- dashcam/package/gnss-logger/gnss-logger.mk
+++ dashcam/package/gnss-logger/gnss-logger.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNSS_LOGGER_VERSION = 1858092f25ffea5ff331dfe58e44732891bfe529
+GNSS_LOGGER_VERSION = cd43f8cf0a688ed3f16e991cc3ada6cd349fa1d7
 GNSS_LOGGER_SITE = ssh://git@bitbucket.org/chr1sniessl/capable_camera_firmware-mirror.git
 GNSS_LOGGER_SITE_METHOD = git
 GNSS_LOGGER_CONF_OPTS = -DCMAKE_INSTALL_PREFIX="/opt/dashcam" \
@@ -15,6 +15,8 @@ GNSS_LOGGER_SUBDIR = gnss
 define GNSS_LOGGER_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/gnss-logger/gnss-logger.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/gnss-logger.service
+	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/gnss-logger/gnss-logger-config.txt \
+		$(TARGET_DIR)/opt/dashcam/gnss-logger-config.txt
 endef
 
 $(eval $(cmake-package))
