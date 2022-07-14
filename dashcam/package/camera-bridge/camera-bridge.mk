5a56a79202abcf7db34d75d3f82cd1a4c9615c58
Chris Niessl <chris.niessl@mydadel.com>
2022-07-14 17:35:08 -0400

    Fix IMX477.json file

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = b95fb4827639bb344332239294cc6f94575570b6
+CAMERA_BRIDGE_VERSION = 1725f3e0b1bfecddb56d7907ad07f9a4a70086c6
 CAMERA_BRIDGE_SITE = ssh://git@bitbucket.org/chr1sniessl/capable_camera_firmware-mirror.git
 CAMERA_BRIDGE_SITE_METHOD = git
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
