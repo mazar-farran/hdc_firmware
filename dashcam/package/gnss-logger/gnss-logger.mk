4be9a1a6856c7f8d05bee55d61000208af990475
Gunnar Ryder <gunnar@hellbender.com>
2022-08-01 16:54:47 +0000

    Add LED Controller package
    - Added LED controller package to build defconfig
    - Added needed package files to build the application into the image

--- dashcam/package/gnss-logger/gnss-logger.mk
+++ dashcam/package/gnss-logger/gnss-logger.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-GNSS_LOGGER_VERSION = f8e63a3aac387bc80896f46078e48f2a9e5707bb
+GNSS_LOGGER_VERSION = dbf14befd80bc19866b4125cefaff38dadd2d3a9
 GNSS_LOGGER_SITE = ssh://git@bitbucket.org/chr1sniessl/capable_camera_firmware-mirror.git
 GNSS_LOGGER_SITE_METHOD = git
 GNSS_LOGGER_CONF_OPTS = -DCMAKE_INSTALL_PREFIX="/opt/dashcam" \
 						-DINSTALL_CONFIG_FILES_PATH="/opt/dashcam/bin/"
 GNSS_LOGGER_DEPENDENCIES = boost json-for-modern-cpp
-GNSS_LOGGER_SUBDIR = gnss
+GNSS_LOGGER_MAKE_OPTS = gnss-logger
 
 define GNSS_LOGGER_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/gnss-logger/gnss-logger.service \
