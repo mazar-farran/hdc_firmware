451a54353539759265fd22d8cd6216eba7ff931f
Chris Niessl <niessl@hellbender.com>
2022-08-16 17:59:39 -0400

    Initial test of configuration files for switching between AP and P2P

---
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_AP.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This brings down any wifi-direct configuration and brings up
+# the original Access point configuration.
+
+killall -9 wpa_supplicant
+
+cp /opt/dashcam/cfg/AP_dhcpcd.conf /etc/dhcpcd.conf
+cp /opt/dashcam/cfg/AP_dnsmasq.conf /etc/dnsmasq.conf
+
+systemctl restart systemd-networkd
+systemctl restart hostapd
+systemctl restart dnsmasq
+
+rm /opt/dashcam/network_mode.txt
+echo "AP" > /opt/dashcam/network_mode.txt
---
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_P2P.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This brings down the Wifi AP on wlan0 and brings up wifi direct
+# Note you will still need to establish a wpa_cli connection
+
+systemctl stop hostapd
+
+cp /opt/dashcam/cfg/P2P_dhcpcd.conf /etc/dhcpcd.conf
+cp /opt/dashcam/cfg/P2P_dnsmasq.conf /etc/dnsmasq.conf
+
+wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant.conf &
+systemctl restart systemd-networkd
+
+rm /opt/dashcam/network_mode.txt
+echo "P2P" > /opt/dashcam/network_mode.txt
