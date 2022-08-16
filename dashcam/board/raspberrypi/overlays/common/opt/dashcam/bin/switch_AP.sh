f640a87a7b13abc327343496d0105a780d82d3ae
Chris Niessl <niessl@hellbender.com>
2022-08-16 19:18:44 -0400

    Potential fix for the reboot issue

--- dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_AP.sh
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_AP.sh
@@ -4,12 +4,16 @@
 # the original Access point configuration.
 
 killall -9 wpa_supplicant
+systemctl stop hostapd
+systemctl stop dhcpcd
+systemctl stop dnsmasq
 
 cp /opt/dashcam/cfg/AP_dhcpcd.conf /etc/dhcpcd.conf
 cp /opt/dashcam/cfg/AP_dnsmasq.conf /etc/dnsmasq.conf
 
 systemctl restart systemd-networkd
 systemctl restart hostapd
+systemctl restart dhcpcd
 systemctl restart dnsmasq
 
 rm /opt/dashcam/network-mode.txt
--- dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_P2P.sh
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/switch_P2P.sh
@@ -3,14 +3,20 @@
 # This brings down the Wifi AP on wlan0 and brings up wifi direct
 # Note you will still need to establish a wpa_cli connection
 
-systemctl stop hostapd
 systemctl stop wpa_supplicant
+systemctl stop hostapd
+systemctl stop dhcpcd
+systemctl stop dnsmasq
 
 cp /opt/dashcam/cfg/P2P_dhcpcd.conf /etc/dhcpcd.conf
 cp /opt/dashcam/cfg/P2P_dnsmasq.conf /etc/dnsmasq.conf
 
 wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant.conf &
+
 systemctl restart systemd-networkd
+systemctl restart dhcpcd
+systemctl restart dnsmasq
+
 
 rm /opt/dashcam/network-mode.txt
 echo "P2P" > /opt/dashcam/network-mode.txt
