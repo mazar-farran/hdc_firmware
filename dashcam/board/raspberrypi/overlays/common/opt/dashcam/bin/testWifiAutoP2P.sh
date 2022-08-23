f203239c0970f68bf9ff5a160da02e8c2223b8c3
Chris Niessl <niessl@hellbender.com>
2022-08-23 14:34:42 -0400

    Add in auto setup/connect

---
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/testWifiAutoP2P.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Debug/test functionality to make sure all associated
+# services can go down and back up.
+
+resultFile="/tmp/results.txt"
+
+touch resultFile
+echo "Switching to P2P" >> $resultFile
+sh /opt/dashcam/bin/switch_P2P.sh >> $resultFile
+sleep 5
+echo "Looking for your phone" >> $resultFile
+sh /opt/dashcam/bin/P2P_connect_nocheck.sh >> $resultFile
+sleep 5
+if test -f "/tmp/CONNECT_SUCCESS"; then
+then
+  echo "Success!" >> $resultFile
+else
+  echo "Failed! Switching back" >> $resultFile
+  sh /opt/dashcam/bin/switch_AP.sh >> $resultFile
+fi
+
