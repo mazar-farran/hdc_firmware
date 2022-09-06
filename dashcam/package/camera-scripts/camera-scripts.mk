c9c690be07341f8ce5875841ae61231d9fe38055
Gunnar Ryder <gunnar@hellbender.com>
2022-09-06 15:26:15 +0000

    Add project scripts
    - Added camera-scripts package to buildroot infrastructure
    - Updated 64-bit defconfig to build defconfig

---
+++ dashcam/package/camera-scripts/camera-scripts.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# camera-scripts
+#
+################################################################################
+
+CAMERA_SCRIPTS_VERSION = 37992d79a9faadcecfb97ed32cd6129558719530
+CAMERA_SCRIPTS_SITE = ssh://git@bitbucket.org/chr1sniessl/capable_camera_firmware-mirror.git
+CAMERA_SCRIPTS_SITE_METHOD = git
+CAMERA_SCRIPTS_SUBDIR = scripts
+
+define CAMERA_SCRIPTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/scripts/* $(TARGET_DIR)/opt/dashcam/bin
+endef
+
+$(eval $(generic-package))
