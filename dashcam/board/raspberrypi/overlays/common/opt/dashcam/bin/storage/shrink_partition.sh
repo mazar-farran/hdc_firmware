e12c0be5b6f16bcdcef90d882ae76384378d55ae
Chris Niessl <niessl@hellbender.com>
2022-09-20 16:02:26 -0400

    Update script with delays to allow time for shutdown of services, and cleaning Use more reliable partition size

--- dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/storage/shrink_partition.sh
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/storage/shrink_partition.sh
@@ -4,9 +4,13 @@
 #
 
 sh /opt/dashcam/bin/stop_all.sh
+sleep 3;
 sh /opt/dashcam/bin/storage/clean_data.sh
+sleep 3;
 umount /dev/mmcblk0p4
-parted /dev/mmcblk0 resizepart 4 772MB
+sleep 1;
+resize2fs /dev/mmcblk0p4 17254
+parted /dev/mmcblk0 resizepart 4 781MB
 resize2fs /dev/mmcblk0p4
 mount /dev/mmcblk0p4
 
