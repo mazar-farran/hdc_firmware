0067658629a66123fa75a2fcbb67826e1efd9bf8
Ari Bruck <ari@Aris-MacBook-Pro.local>
2022-09-22 12:23:05 -0500

    bumping camera package version to include quality=80

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = d8ee0e4a2e88ce5e6409780f006a9d1a6a0c4e19
+CAMERA_BRIDGE_VERSION = 50168b8c50c8601d8260a11cc6149729a89b7313
 CAMERA_BRIDGE_SITE = $(call github,CapableRobot,capable_camera_firmware,$(CAMERA_BRIDGE_VERSION))
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
                           -DINSTALL_CONFIG_FILES_PATH="/opt/dashcam/bin/"
