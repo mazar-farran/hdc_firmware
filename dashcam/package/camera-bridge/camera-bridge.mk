d1c080b314d07302ad49c11cd538cef4328ccaa6
cbillett <charles@streamingfast.io>
2023-05-15 14:04:34 -0400

    new camera bridge setup

--- dashcam/package/camera-bridge/camera-bridge.mk
+++ dashcam/package/camera-bridge/camera-bridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAMERA_BRIDGE_VERSION = 85cf1bb9ebbe4f31e826325204e1aa26f2cbb03d
+CAMERA_BRIDGE_VERSION = 377c294dc3c9a663ad23d28bc4e562b49c7e4780
 CAMERA_BRIDGE_SITE = git@github.com:Hivemapper/camera_bridge.git
 CAMERA_BRIDGE_SITE_METHOD = git
 CAMERA_BRIDGE_CONF_OPTS = -DENABLE_OPENCV=0 -DCMAKE_INSTALL_PREFIX="/opt/dashcam"\
@@ -17,8 +17,10 @@ CAMERA_BRIDGE_DEPENDENCIES = boost libcamera jpeg-turbo json-for-modern-cpp
 define CAMERA_BRIDGE_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-bridge/camera-bridge.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/camera-bridge.service
-	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-bridge/camera-preview.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/camera-preview.service
+	$(INSTALL) -D -m 755 $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-bridge/camera_bridge.sh \
+		$(TARGET_DIR)/opt/dashcam/bin/camera_bridge.sh
+	$(INSTALL) -D -m 644 $(BR2_EXTERNAL_DASHCAM_PATH)/package/camera-bridge/camera_bridge_config.json \
+		$(TARGET_DIR)/opt/dashcam/bin/camera_bridge_config.json
 endef
 
 $(eval $(cmake-package))
