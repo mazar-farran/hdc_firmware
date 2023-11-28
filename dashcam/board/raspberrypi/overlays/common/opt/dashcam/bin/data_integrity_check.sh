ad026d3ba4595b18af0fa370f2a2b4181cc468e1
Aleksei Punov <design.qs@gmail.com>
2023-11-27 17:50:03 -0800

    ODC API v3.7.5

--- dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/data_integrity_check.sh
+++ dashcam/board/raspberrypi/overlays/common/opt/dashcam/bin/data_integrity_check.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 framekm_folder metadata_folder log_file_path"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 framekm_folder metadata_folder log_file_path events_file_path"
     exit 1
 fi
 
 folder1="$1"
 folder2="$2"
-file_path="$3"
-max_file_size=$((2 * 1024 * 1024))
-min_file_size=$((100 * 1024))
+log_file_path="$3"
+events_file_path="$4"
+max_file_size=$((2 * 1024 * 1024)) # 2MB
+min_file_size=$((100 * 1024)) # 100KB
 
 # Remove all empty files from folder2
 find "$folder2" -type f -empty -exec rm -f {} +
@@ -34,23 +35,19 @@ find "$folder1" -type f | while read -r file; do
     fi
 done
 
-# Process JSON files in folder2
-# If json exists in folder2 but corresponding file doesn't exist in folder1, remove json in folder2
-find "$folder2" -type f -name "*.json" | while read -r json; do
-    base_name=$(basename "$json" .json)
-    file="${folder1}/${base_name}"
-    
-    if [[ ! -e "$file" ]]; then
-        echo "Removing: $json"
-        rm -f "$json"
-    fi
-done
-
 # Check and truncate the file if it's larger than 2 MB
-if [ -e "$file_path" ]; then
-    file_size=$(stat -c%s "$file_path")
+if [ -e "$log_file_path" ]; then
+    file_size=$(stat -c%s "$log_file_path")
     if [ $file_size -gt $max_file_size ]; then
-        tail -c $max_file_size "$file_path" > "${file_path}.tmp" && mv "${file_path}.tmp" "$file_path"
-        echo "Truncated $file_path to the last 2 MB"
+        tail -c $max_file_size "$log_file_path" > "${log_file_path}.tmp" && mv "${log_file_path}.tmp" "$log_file_path"
+        echo "Truncated $log_file_path to the last 2 MB"
+    fi
+fi
+
+if [ -e "$events_file_path" ]; then
+    file_size=$(stat -c%s "$events_file_path")
+    if [ $file_size -gt $max_file_size ]; then
+        tail -c $max_file_size "$events_file_path" > "${events_file_path}.tmp" && mv "${events_file_path}.tmp" "$events_file_path"
+        echo "Truncated $events_file_path to the last 2 MB"
     fi
 fi
