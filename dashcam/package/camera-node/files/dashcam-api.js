2c227a8b552f277e2984496697acb3f1e9bc3363
Aleksei Punov <design.qs@gmail.com>
2024-02-01 00:16:40 -0800

    Uptick to 3.9.2

--- dashcam/package/camera-node/files/dashcam-api.js
+++ dashcam/package/camera-node/files/dashcam-api.js
@@ -120,7 +120,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(9020), exports);
-exports.API_VERSION = '3.9.1';
+exports.API_VERSION = '3.9.2';
 const isDev = () => {
     return false;
 };
@@ -64660,7 +64660,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.isDev = exports.API_VERSION = void 0;
 __exportStar(__nccwpck_require__(4484), exports);
-exports.API_VERSION = '3.9.1';
+exports.API_VERSION = '3.9.2';
 const isDev = () => {
     return false;
 };
