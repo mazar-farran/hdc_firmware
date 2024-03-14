8af24115924977b4bf2a978963ad2757c544f84d
Mazar Farran <mazar@hivemapper.com>
2024-03-14 12:20:06 -0700

    Workaround for ncc and sqlite3_node issues

--- dashcam/package/camera-node/camera-node.mk
+++ dashcam/package/camera-node/camera-node.mk
@@ -9,10 +9,32 @@ CAMERA_NODE_SITE = git@github.com:Hivemapper/odc-api.git
 CAMERA_NODE_SITE_METHOD = git
 CAMERA_NODE_DEPENDENCIES = host-nodejs nodejs
 
+# Define NPM for other packages to use
+# $(NPM) does not work. ncc will build incorrectly.
+NPM_HOST = $(TARGET_CONFIGURE_OPTS) \
+	LDFLAGS="$(NODEJS_LDFLAGS)" \
+	LD="$(TARGET_CXX)" \
+	$(HOST_DIR)/bin/npm
+
+ARCH_STRING=napi-v6-linux-glibc-x64
+
+# Note: We cannot build the binary node_sqlite3.node correctly
+#       because we are on a different arch than the target.
+#       So instead we will use the node_sqlite3.node found in
+#       raspberrypi/overlays
 define CAMERA_NODE_BUILD_CMDS
 	mkdir -p $(@D)/node_modules
-	$(NPM) install --prefix $(@D)
-	$(NPM) run --prefix $(@D) compile-gh --camera=hdc
+	$(NPM_HOST) install --prefix $(@D)
+	$(NPM_HOST) run --prefix $(@D) compile-gh --camera=hdc
+
+	# Check for expected string and fail if it's not there.
+	# This is better than having a borked firmware build.
+	# If this fails, you're probably compiling on a new arch and need
+	# to change ARCH_STRING to match compiled/lib/binding/<name>.
+	grep $(ARCH_STRING) $(@D)/compiled/odc-api-hdc.js
+
+	# change to match the node_sqlite3 path in overlays
+	sed -i 's/$(ARCH_STRING)/napi-v6-darwin-unknown-arm64/g' $(@D)/compiled/odc-api-hdc.js
 endef
 
 define CAMERA_NODE_INSTALL_TARGET_CMDS
